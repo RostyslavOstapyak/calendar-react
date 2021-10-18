fedfb00cbf8cf768750cd0ffb16d0f452f142f0b
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-18 22:39:43 +0300

    header buttons today, prevWeek, nextWeek -- done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import moment from "moment";
 import Header from "./components/header/Header.jsx";
 import Calendar from "./components/calendar/Calendar.jsx";
 
@@ -11,9 +12,30 @@ const App = () => {
 
   const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
 
+  const goNextWeek = () => {
+    const copyData = new Date(weekStartDate);
+    let nextWeekStartDate = moment(copyData).get("date");
+    setWeekStartDate(moment(copyData).set("date", nextWeekStartDate + 7)._d);
+  };
+
+  const goPrevWeek = () => {
+    const copyData = new Date(weekStartDate);
+    let nextWeekStartDate = moment(copyData).get("date");
+    setWeekStartDate(moment(copyData).set("date", nextWeekStartDate - 7)._d);
+  };
+
+  const goToday = () => {
+    setWeekStartDate(new Date());
+  };
+
   return (
     <>
-      <Header />
+      <Header
+        weekDates={weekDates}
+        goNextWeek={goNextWeek}
+        goPrevWeek={goPrevWeek}
+        goToday={goToday}
+      />
       <Calendar weekDates={weekDates} />
     </>
   );
